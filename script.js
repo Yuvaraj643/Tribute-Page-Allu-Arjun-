30dfc7555ca17fa2194e5bf157b8916eab12477a
Yuvaraj V <vyuvarajnani2001@gmail.com>
2023-04-10 22:22:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const bgAudio = document.getElementById("background-audio");
+const myVideo = document.getElementById("my-video");
+
+myVideo.addEventListener('play', () => {
+    bgAudio.pause();
+  });
+
+  myVideo.addEventListener('pause', () => {
+    bgAudio.play();
+  });
+
+  myVideo.addEventListener('ended', () => {
+    bgAudio.play();
+});
